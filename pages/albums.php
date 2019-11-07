efccf39d59346eb03439a9824b0cebc5fde165b9
Lucas Andrade <lucascorporation@live.com>
2019-11-07 01:13:23 -0300

    listandos os Albuns

--- pages/albums.php
+++ pages/albums.php
@@ -1,2 +1,18 @@
-<?php
-echo "albums";
+<h2 class="text-center text-warning"><i class="fas fa-heartbeat"></i><b>Álbuns</b></h2>
+
+<div class="row">
+    <?php
+    for ($i=0; $i <10 ; $i++){  
+ 
+    ?>
+    <div class="col-md-3">
+
+        <a href="">
+        <img class="card-img-top"  src="albums/74237740_2418421971527940_2729624893984342016_o.jpg" alt="">
+        <h3 class="text-primary">Album <?=$i+1?></h3>
+        </a>
+    </div>
+    <?php
+        }
+    ?>
+</div>
